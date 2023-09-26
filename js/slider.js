9a79c01ea966b3eba2bc286c367c17665d842ec8
GoOdLoG <labuskiypetro@gmail.com>
2023-09-26 12:32:10 +0300

    add slider nav

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,5 @@
 const rad = document.getElementsByClassName('slider-image')
+const rad1 = document.getElementsByClassName('slider-radio')
 const img_width = -(500 + 50);
 const steep = [-img_width, 0, img_width, 2*img_width]
 
@@ -6,6 +7,10 @@ function position(active){
     for(let i = 0; i < rad.length; i++){
         rad[i].classList.remove('slider-image_active');
         rad[i].style.left = steep[active]+ "px"
+
+        rad1[i].style.opacity = '40%';
     }
+    
     rad[active].classList.add('slider-image_active')
+    rad1[active].style.opacity = '100%';
 }
