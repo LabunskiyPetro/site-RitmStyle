f9f85985c53d2248ea1f43bdee610696dd497a7b
GoOdLoG <labuskiypetro@gmail.com>
2023-09-27 01:37:03 +0300

    corrects slider

--- js/slider.js
+++ js/slider.js
@@ -1,16 +1,15 @@
-const rad = document.getElementsByClassName('slider-image')
-const rad1 = document.getElementsByClassName('slider-radio')
+const rad = document.getElementsByClassName('slider__image')
+const rad1 = document.getElementsByClassName('slider__radio')
 const img_width = -(500 + 50);
-const steep = [-img_width, 0, img_width, 2*img_width]
 
 function position(active){
     for(let i = 0; i < rad.length; i++){
-        rad[i].classList.remove('slider-image_active');
-        rad[i].style.left = steep[active]+ "px"
-
+        rad[i].classList.remove('slider__image_active');
+        rad[i].style.left = img_width * (active - 1)+ "px"
+        
         rad1[i].style.opacity = '40%';
     }
     
-    rad[active].classList.add('slider-image_active')
+    rad[active].classList.add('slider__image_active')
     rad1[active].style.opacity = '100%';
 }
