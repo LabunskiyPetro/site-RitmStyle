da2bf77d75640e7204408b032850edae111590eb
GoOdLoG <labuskiypetro@gmail.com>
2023-09-27 22:58:11 +0300

    fix main

--- js/slider.js
+++ js/slider.js
@@ -1,15 +1,17 @@
-const rad = document.getElementsByClassName('slider__image')
-const rad1 = document.getElementsByClassName('slider__radio')
+const item = document.getElementsByClassName('slider__item')
+const radio = document.getElementsByClassName('slider__radio')
 const img_width = -(500 + 50);
 
 function position(active){
-    for(let i = 0; i < rad.length; i++){
-        rad[i].classList.remove('slider__image_active');
-        rad[i].style.left = img_width * (active - 1)+ "px"
-        
-        rad1[i].style.opacity = '40%';
+    for(let i = 0; i < item.length; i++){
+        if(active != i){
+            item[i].classList.remove('slider__item_active');
+            radio[i].style.opacity = '40%';
+        }
+        else{
+            item[i].classList.add('slider__item_active')
+            radio[i].style.opacity = '100%';
+        }
+        item[i].style.left = img_width * (active - 1)+ "px"
     }
-    
-    rad[active].classList.add('slider__image_active')
-    rad1[active].style.opacity = '100%';
 }
